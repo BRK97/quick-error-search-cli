b8d45f5a99ac6c0d883ec7a79bce437c00c50ede
BRK97 <73640959+BRK97@users.noreply.github.com>
2022-05-14 19:17:52 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 let temp;
 
 const findSolutions = async (errCode) => {
-    let url = `https://www.google.co.il/search?q=${errCode}`
+    let url = `https://www.google.com/search?q=${errCode}`
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
